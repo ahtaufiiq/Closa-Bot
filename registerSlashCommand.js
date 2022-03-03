6321445ecccf28745680fddad554f186000fee92
Ahmad Taufiq <ataufiq665@gmail.com>
2022-03-04 04:46:27 +0700

    get list command from commands folder

--- registerSlashCommand.js
+++ registerSlashCommand.js
@@ -1,12 +1,17 @@
 const {REST} = require('@discordjs/rest')
 const {Routes} = require('discord-api-types/v9')
-
+const fs = require('fs')
 const {CLIENT_ID,GUILD_ID,TOKEN} = require('./helpers/config')
 
-const commands = [{
-    name: 'ping',
-    description: 'Replies with Pong!'
-  }]; 
+const commands = []; 
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	commands.push(command.data.toJSON())
+}
   
   const rest = new REST({ version: '9' }).setToken(TOKEN);
   
