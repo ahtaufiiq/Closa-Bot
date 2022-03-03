d90bef6b6da39fff02c6a21e885e0519926bb0e9
Ahmad Taufiq <ataufiq665@gmail.com>
2022-03-03 12:46:00 +0700

    basic bot

---
+++ registerSlashCommand.js
@@ -0,0 +1,26 @@
+const {REST} = require('@discordjs/rest')
+const {Routes} = require('discord-api-types/v9')
+
+const {CLIENT_ID,GUILD_ID,TOKEN} = require('./helpers/config')
+
+const commands = [{
+    name: 'ping',
+    description: 'Replies with Pong!'
+  }]; 
+  
+  const rest = new REST({ version: '9' }).setToken(TOKEN);
+  
+  (async () => {
+    try {
+      console.log('Started refreshing application (/) commands.');
+  
+      await rest.put(
+        Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID),
+        { body: commands },
+      );
+  
+      console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+      console.error(error);
+    }
+  })();
