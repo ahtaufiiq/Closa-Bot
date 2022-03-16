5a0f00628560a0445d6a099b579fda6dd9fd38d0
Ahmad Taufiq <ataufiq665@gmail.com>
2022-03-16 20:09:07 +0700

    highlight reminder

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -4,25 +4,48 @@ const { CHANNEL_REMINDER , CHANNEL_HIGHLIGHT, CHANNEL_TODO} = require("../helper
 const supabase = require("../helpers/supabaseClient");
 const Time = require("../helpers/time");
 const DailyStreakMessage = require("../views/DailyStreakMessage");
-
+const schedule = require('node-schedule');
 module.exports = {
 	name: 'messageCreate',
 	async execute(msg) {
 		const ChannelReminder = msg.guild.channels.cache.get(CHANNEL_REMINDER)
 		switch (msg.channelId) {
 			case CHANNEL_HIGHLIGHT:
+				const patternTime = /\d+[.:]\d+/
+				const patternEmoji = /^🔆/
+				if (patternEmoji.test(msg.content.trimStart())) {
+                    
+					if (patternTime.test(msg.content)) {
+						const time = msg.content.match(patternTime)[0]
+                        const [hours,minutes] = time.split(/[.:]/)
+						const date = new Date()
+						date.setHours(hours)
+						date.setMinutes(minutes-10)
+						RequestAxios.post('highlights', {
+							description: msg.content,
+							UserId: msg.author.id
+						})
+						.then(()=>{
+							
+							supabase.from('Users')
+								.update({last_highlight:Time.getDate().toISOString().substring(0,10)})
+								.eq('id',msg.author.id)
+								.then()
+							
+							const ruleReminderHighlight = new schedule.RecurrenceRule();
+							ruleReminderHighlight.hour = Time.minus7Hours(date.getHours())
+							ruleReminderHighlight.minute = Number(date.getMinutes()) 
+							const reminderHighlight = schedule.scheduleJob(ruleReminderHighlight,function () {
+								ChannelReminder.send(`Hi ${msg.author} reminder: ${msg.content} `)
+							})
+						})
+					}else{
+						msg.delete()
+						ChannelReminder.send(`Hi ${msg.author} please __add a specific time__ to your highlight to stay accountable!
+For example: 🔆 read 25 page of book **at 19.00**`)
+					}
+				}
 				
-				RequestAxios.post('highlights', {
-					description: msg.content,
-					UserId: msg.author.id
-				})
-				.then(()=>{
-					
-					supabase.from('Users')
-						.update({last_highlight:Time.getDate().toISOString().substring(0,10)})
-						.eq('id',msg.author.id)
-						.then()
-				})
 					
 				break;
 			case CHANNEL_TODO:
