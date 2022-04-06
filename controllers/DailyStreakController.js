11759118f5dc32d7440ab1d4bcdb015b57bfbab7
Ahmad Taufiq <ataufiq665@gmail.com>
2022-04-06 14:57:14 +0700

    send congratulations achieve daily streak only first time

--- controllers/DailyStreakController.js
+++ controllers/DailyStreakController.js
@@ -3,24 +3,19 @@ const MemberController = require('./MemberController');
 const {ROLE_7STREAK,ROLE_30STREAK,ROLE_100STREAK,ROLE_365STREAK} = require('../helpers/config')
 class DailyStreakController {
     
-    static achieveDailyStreak(bot,ChannelReminder,dailyStreak,author){
-        switch (dailyStreak) {
-            case 7:
-               ChannelReminder.send({content:`Welcome to <@&${ROLE_7STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(7)]})
-               MemberController.addRole(bot, author.id, ROLE_7STREAK)
-                break;
-            case 30:
-                ChannelReminder.send({content:`Welcome to <@&${ROLE_30STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(30)]})
-                MemberController.addRole(bot, author.id, ROLE_30STREAK)
-                break;
-            case 100:
-                ChannelReminder.send({content:`Welcome to <@&${ROLE_100STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(100)]})
-                MemberController.addRole(bot, author.id, ROLE_100STREAK)
-                break;
-            case 365:
-                ChannelReminder.send({content:`Welcome to <@&${ROLE_365STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(365)]})
-                MemberController.addRole(bot, author.id, ROLE_365STREAK)
-                break;
+    static achieveDailyStreak(bot,ChannelReminder,dailyStreak,longestStreak,author){
+        if (dailyStreak === 7 && dailyStreak === longestStreak) {
+            ChannelReminder.send({content:`Welcome to <@&${ROLE_7STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(7)]})
+            MemberController.addRole(bot, author.id, ROLE_7STREAK)
+        }else if (dailyStreak === 30 && dailyStreak === longestStreak) {
+            ChannelReminder.send({content:`Welcome to <@&${ROLE_30STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(30)]})
+            MemberController.addRole(bot, author.id, ROLE_30STREAK)
+        }else if (dailyStreak === 100 && dailyStreak === longestStreak) {
+            ChannelReminder.send({content:`Welcome to <@&${ROLE_100STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(100)]})
+            MemberController.addRole(bot, author.id, ROLE_100STREAK)
+        }else if (dailyStreak === 365 && dailyStreak === longestStreak) {
+            ChannelReminder.send({content:`Welcome to <@&${ROLE_365STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(365)]})
+            MemberController.addRole(bot, author.id, ROLE_365STREAK)
         }
     }
 }
