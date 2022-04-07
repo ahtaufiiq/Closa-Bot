c288052a3f13746d8437ec662cd43cb805fc64c1
Ahmad Taufiq <ataufiq665@gmail.com>
2022-04-07 15:23:27 +0700

    add import MemberController and getIdTopics for add role reaction

--- events/messageReactionAdd.js
+++ events/messageReactionAdd.js
@@ -1,15 +1,11 @@
-const DailyStreakController = require("../controllers/DailyStreakController");
-const RequestAxios = require("../helpers/axios");
-const { CHANNEL_REMINDER , CHANNEL_HIGHLIGHT, CHANNEL_TODO,CHANNEL_STREAK,GUILD_ID,CHANNEL_GOALS} = require("../helpers/config");
-const supabase = require("../helpers/supabaseClient");
-const Time = require("../helpers/time");
-const DailyStreakMessage = require("../views/DailyStreakMessage");
-const schedule = require('node-schedule');
+const MemberController = require("../controllers/MemberController");
+const getIdTopics = require("../helpers/getIdTopic");
 
 module.exports = {
 	name: 'messageReactionAdd',
 	async (reaction, user) {
 		// handle only message with this id
+			
 		if(reaction.message.id !== "960790258256064542") return
 		if (reaction.partial) {
 			try {
