a02980ada0e4bd81ffccda53ad7ad4185bd3365d
Ahmad Taufiq <ataufiq665@gmail.com>
2022-07-14 11:49:33 +0700

    set title thread at channel progress (exclude emoji)

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -120,8 +120,11 @@ For example: 🔆 read 25 page of book **at 19.00**`)
 so, you can learn or sharing from each others.`)
 					return
 				}
+				let titleProgress = `${string.trimStart().split('\n')[0]}`
+				if(FormatString.notCharacter(titleProgress[0])) titleProgress = titleProgress.slice(1).trimStart()
+				
 				const threadProgress = await msg.startThread({
-					name: FormatString.truncateString(msg.content,90),
+					name: FormatString.truncateString(titleProgress,90),
 				});
 				
 				const { data, error } = await supabase
