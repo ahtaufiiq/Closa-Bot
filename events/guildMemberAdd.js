115768654e2e0d1ad39433b918a858d0d446207c
Ahmad Taufiq <ataufiq665@gmail.com>
2022-08-13 17:26:27 +0700

    create notification thread when user joined server

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,4 +1,6 @@
+const ChannelController = require("../controllers/ChannelController");
 const RequestAxios = require("../helpers/axios");
+const { CHANNEL_NOTIFICATION } = require("../helpers/config");
 const supabase = require("../helpers/supabaseClient");
 const Time = require("../helpers/time");
 
@@ -8,16 +10,23 @@ module.exports = {
 		RequestAxios.get('users/'+member.user.id)
 			.then(data=>{
 				if (!data) {
-					supabase.from("Users")
-						.insert([{
-							id:member.user.id,
-							username:member.user.username,
-							name:member.user.username,
-							total_days:0,
-							total_points:0,
-							last_active:Time.getTodayDateOnly()
-						}])
-						.then()
+					const channelNotifications = ChannelController.getChannel(member.client,CHANNEL_NOTIFICATION)
+					channelNotifications.send(`${member.user}`)
+					.then(msg=>{
+						supabase.from("Users")
+							.insert([{
+								id:member.user.id,
+								username:member.user.username,
+								name:member.user.username,
+								notification_id:msg.id,
+								total_days:0,
+								total_points:0,
+								last_active:Time.getTodayDateOnly()
+							}])
+							.then()
+						ChannelController.createThread(msg,member.user.username)
+					})
+					
 					
 				}
 			})
