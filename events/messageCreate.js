94208aa0ba71a7b30a075fb3dbeeb45b2ed55e31
Ahmad Taufiq <ataufiq665@gmail.com>
2022-09-15 06:21:20 +0700

    comment generate referral

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -133,9 +133,9 @@ module.exports = {
 so, you can learn or sharing from each others.`)
 					return
 				}
-				if(ReferralCodeController.isTimeToGenerateReferral()){
-					ReferralCodeController.generateReferral(msg.client,msg.author.id)
-				}
+				// if(ReferralCodeController.isTimeToGenerateReferral()){
+				// 	ReferralCodeController.generateReferral(msg.client,msg.author.id)
+				// }
 				let titleProgress = `${msg.content.trimStart().split('\n')[0]}`
 				if(FormatString.notCharacter(titleProgress[0])) titleProgress = titleProgress.slice(1).trimStart()
 
