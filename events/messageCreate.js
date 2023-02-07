407658abed4c389294380364be20942b748ae303
Ahmad Taufiq <ataufiq665@gmail.com>
2023-02-07 13:00:25 +0700

    fix error

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -171,7 +171,7 @@ so, you can learn or sharing from each others.`,
 					const thread = await channel.threads.fetch(data.goalId);
 					goalName = thread.name.split('by')[0]
 					thread.send(
-						GoalMessage.shareProgress(msg,files,data.body.totalDay)
+						GoalMessage.shareProgress(msg,files,data.totalDay)
 					)
 				}else{
 					ChannelController.sendToNotification(
