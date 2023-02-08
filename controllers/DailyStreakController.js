7582c17877f2e8e667ba8afa432d35fd2ff2f59d
Ahmad Taufiq <ataufiq665@gmail.com>
2023-02-08 15:13:53 +0700

    add image achievement 7,30,100 streak badge

--- controllers/DailyStreakController.js
+++ controllers/DailyStreakController.js
@@ -28,6 +28,10 @@ class DailyStreakController {
             data = {content:`Welcome to <@&${ROLE_365STREAK}> ${author}! :partying_face: :tada: `,embeds:[DailyStreakMessage.notifyDailyStreak(365)]}
             MemberController.addRole(bot, author.id, ROLE_365STREAK)
         }
+
+		const buffer = await GenerateImage.streakBadge(dailyStreak,author)
+		const attachment = new MessageAttachment(buffer,`streak_badge_${msg.author.username}.png`)
+		data.files = [attachment]
 		
 		const msg = await ChannelReminder.send(data)
 		ChannelController.createThread(msg,`Congrats ${author.username}`)
