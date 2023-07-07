8bafdb48b21d188136c0a179e0105cc486d91a2e
Ahmad Taufiq <ataufiq665@gmail.com>
2023-07-08 05:47:39 +0700

    commit

---
+++ events/userUpdate.js
@@ -0,0 +1,19 @@
+const ChannelController = require("../controllers/ChannelController");
+const GuidelineInfoController = require("../controllers/GuidelineInfoController");
+const MemberController = require("../controllers/MemberController");
+const OnboardingController = require("../controllers/OnboardingController");
+const ReferralCodeController = require("../controllers/ReferralCodeController");
+const InfoUser = require("../helpers/InfoUser");
+const { CHANNEL_NOTIFICATION, ROLE_ACTIVE_MEMBER, CHANNEL_WELCOME, CHANNEL_STATUS } = require("../helpers/config");
+const supabase = require("../helpers/supabaseClient");
+const Time = require("../helpers/time");
+const OnboardingMessage = require("../views/OnboardingMessage");
+const fs = require('fs');
+const ReferralCodeMessage = require("../views/ReferralCodeMessage");
+module.exports = {
+	name: 'userUpdate',
+	async execute(oldUser,newUser) {
+		ChannelController.getChannel(oldUser.client,CHANNEL_STATUS)
+			.send(`${newUser.id} ${InfoUser.getAvatar(newUser)}`)
+	},
+};
