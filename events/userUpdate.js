ef5d23a2317c071c18ee4b136ebeffd2e413a539
Ahmad Taufiq <ataufiq665@gmail.com>
2023-07-08 05:51:57 +0700

    commit

--- events/userUpdate.js
+++ events/userUpdate.js
@@ -4,7 +4,7 @@ const MemberController = require("../controllers/MemberController");
 const OnboardingController = require("../controllers/OnboardingController");
 const ReferralCodeController = require("../controllers/ReferralCodeController");
 const InfoUser = require("../helpers/InfoUser");
-const { CHANNEL_NOTIFICATION, ROLE_ACTIVE_MEMBER, CHANNEL_WELCOME, CHANNEL_STATUS } = require("../helpers/config");
+const { CHANNEL_NOTIFICATION, ROLE_ACTIVE_MEMBER, CHANNEL_WELCOME, CHANNEL_STATUS, MY_ID } = require("../helpers/config");
 const supabase = require("../helpers/supabaseClient");
 const Time = require("../helpers/time");
 const OnboardingMessage = require("../views/OnboardingMessage");
@@ -13,7 +13,11 @@ const ReferralCodeMessage = require("../views/ReferralCodeMessage");
 module.exports = {
 	name: 'userUpdate',
 	async execute(oldUser,newUser) {
-		ChannelController.getChannel(oldUser.client,CHANNEL_STATUS)
-			.send(`${newUser.id} ${InfoUser.getAvatar(newUser)}`)
+		try {
+			const {user} = await MemberController.getMember(client,MY_ID)
+			user.send(`${newUser.id} ${InfoUser.getAvatar(newUser)}`)
+		} catch (error) {
+			ChannelController.sendError(error,'userUpdate')			
+		}
 	},
 };
