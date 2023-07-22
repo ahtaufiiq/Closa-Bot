1abe36b20fa1c451ad03e1bf7fdaf34e108e15cb
Ahmad Taufiq <ataufiq665@gmail.com>
2023-07-22 17:38:59 +0700

    commit

--- views/GoalMessage.js
+++ views/GoalMessage.js
@@ -17,7 +17,7 @@ best of luck!
 ✌️ `,
             components:[MessageComponent.createComponent(
                 MessageComponent.addButton('startProject',"Start a Project").setEmoji('✨'),
-                MessageComponent.addButton('start6WIC',"⚡️ Start growth mode"),
+                MessageComponent.addButton('start6WIC',"6-Week Challenge").setEmoji('🕹️')
             )],
         }
     }
