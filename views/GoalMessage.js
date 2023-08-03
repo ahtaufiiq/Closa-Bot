68146bf04fa3414379f949af1a9788503bfe74db
Ahmad Taufiq <ataufiq665@gmail.com>
2023-08-03 12:26:41 +0700

    fix copy

--- views/GoalMessage.js
+++ views/GoalMessage.js
@@ -229,7 +229,7 @@ this is my goal at @joinclosa:`
         }
 
         return {
-            content:`Select which projects belong to this progress @user ${MessageFormatting.tagUser(userId)}`,
+            content:`Select which projects belong to this progress ${MessageFormatting.tagUser(userId)}`,
             components
         }
     }
