489bf675ae6632b196135dda794b7f010e0f93b4
Ahmad Taufiq <ataufiq665@gmail.com>
2023-10-22 16:43:24 +0700

    commit

--- helpers/DiscordWebhook.js
+++ helpers/DiscordWebhook.js
@@ -2,9 +2,9 @@ const { WebhookClient } = require("discord.js");
 
 class DiscordWebhook {
     static sendError(error,data='error'){
-        // const webhookClient = new WebhookClient({ url:"https://discord.com/api/webhooks/953519981629419540/5PQwLXEB-Xxh5nuwOANNRUdddt1UTqsCay-TRRVocN-_lV6mXSoSI7KkZX7xiC8PDh1E" });
-        // webhookClient.send(`${data}: ${error}`)
-        console.log(error,data);
+        const webhookClient = new WebhookClient({ url:"https://discord.com/api/webhooks/953519981629419540/5PQwLXEB-Xxh5nuwOANNRUdddt1UTqsCay-TRRVocN-_lV6mXSoSI7KkZX7xiC8PDh1E" });
+        webhookClient.send(`${data}: ${error}`)
+        // console.log(error,data);
     }
 }
 
